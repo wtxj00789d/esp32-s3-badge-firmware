da858cd4bab9ea046649250f1f501c68b4a489f2
Aries <wtxj00789d@gmail.com>
2026-05-09 02:07:38 +0800

    build: consolidate badge settings shim

--- main/audio/settings.h
+++ main/audio/settings.h
@@ -1,28 +1,3 @@
 #pragma once
 
-#include <cstdint>
-#include <string>
-
-class Settings {
-public:
-    Settings(const std::string& ns, bool read_write = false)
-        : ns_(ns), read_write_(read_write)
-    {
-    }
-
-    int32_t GetInt(const std::string& key, int32_t default_value = 0)
-    {
-        (void)key;
-        return default_value;
-    }
-
-    void SetInt(const std::string& key, int32_t value)
-    {
-        (void)key;
-        (void)value;
-    }
-
-private:
-    std::string ns_;
-    bool read_write_;
-};
+#include "../settings.h"
--- main/settings.h
+++ main/settings.h
@@ -1,28 +1,61 @@
-#ifndef SETTINGS_H
-#define SETTINGS_H
+#pragma once
 
+#include <cstdint>
 #include <string>
-#include <nvs_flash.h>
 
 class Settings {
 public:
-    Settings(const std::string& ns, bool read_write = false);
-    ~Settings();
+    Settings(const std::string& ns, bool read_write = false)
+        : ns_(ns), read_write_(read_write)
+    {
+    }
 
-    std::string GetString(const std::string& key, const std::string& default_value = "");
-    void SetString(const std::string& key, const std::string& value);
-    int32_t GetInt(const std::string& key, int32_t default_value = 0);
-    void SetInt(const std::string& key, int32_t value);
-    bool GetBool(const std::string& key, bool default_value = false);
-    void SetBool(const std::string& key, bool value);
-    void EraseKey(const std::string& key);
-    void EraseAll();
+    ~Settings() = default;
+
+    std::string GetString(const std::string& key, const std::string& default_value = "")
+    {
+        (void)key;
+        return default_value;
+    }
+
+    void SetString(const std::string& key, const std::string& value)
+    {
+        (void)key;
+        (void)value;
+    }
+
+    int32_t GetInt(const std::string& key, int32_t default_value = 0)
+    {
+        (void)key;
+        return default_value;
+    }
+
+    void SetInt(const std::string& key, int32_t value)
+    {
+        (void)key;
+        (void)value;
+    }
+
+    bool GetBool(const std::string& key, bool default_value = false)
+    {
+        (void)key;
+        return default_value;
+    }
+
+    void SetBool(const std::string& key, bool value)
+    {
+        (void)key;
+        (void)value;
+    }
+
+    void EraseKey(const std::string& key)
+    {
+        (void)key;
+    }
+
+    void EraseAll() {}
 
 private:
     std::string ns_;
-    nvs_handle_t nvs_handle_ = 0;
-    bool read_write_ = false;
-    bool dirty_ = false;
+    bool read_write_;
 };
-
-#endif
