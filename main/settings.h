bc6fcd3eaed865246a370ee1270bd1474a02b51e
Aries <wtxj00789d@gmail.com>
2026-05-09 12:07:35 +0800

    build: restore nvs settings for badge firmware

--- main/settings.h
+++ main/settings.h
@@ -1,61 +1,28 @@
-#pragma once
+#ifndef SETTINGS_H
+#define SETTINGS_H
 
-#include <cstdint>
+#include <nvs_flash.h>
 #include <string>
 
 class Settings {
 public:
-    Settings(const std::string& ns, bool read_write = false)
-        : ns_(ns), read_write_(read_write)
-    {
-    }
+    Settings(const std::string& ns, bool read_write = false);
+    ~Settings();
 
-    ~Settings() = default;
-
-    std::string GetString(const std::string& key, const std::string& default_value = "")
-    {
-        (void)key;
-        return default_value;
-    }
-
-    void SetString(const std::string& key, const std::string& value)
-    {
-        (void)key;
-        (void)value;
-    }
-
-    int32_t GetInt(const std::string& key, int32_t default_value = 0)
-    {
-        (void)key;
-        return default_value;
-    }
-
-    void SetInt(const std::string& key, int32_t value)
-    {
-        (void)key;
-        (void)value;
-    }
-
-    bool GetBool(const std::string& key, bool default_value = false)
-    {
-        (void)key;
-        return default_value;
-    }
-
-    void SetBool(const std::string& key, bool value)
-    {
-        (void)key;
-        (void)value;
-    }
-
-    void EraseKey(const std::string& key)
-    {
-        (void)key;
-    }
-
-    void EraseAll() {}
+    std::string GetString(const std::string& key, const std::string& default_value = "");
+    void SetString(const std::string& key, const std::string& value);
+    int32_t GetInt(const std::string& key, int32_t default_value = 0);
+    void SetInt(const std::string& key, int32_t value);
+    bool GetBool(const std::string& key, bool default_value = false);
+    void SetBool(const std::string& key, bool value);
+    void EraseKey(const std::string& key);
+    void EraseAll();
 
 private:
     std::string ns_;
-    bool read_write_;
+    nvs_handle_t nvs_handle_ = 0;
+    bool read_write_ = false;
+    bool dirty_ = false;
 };
+
+#endif
